c6f7386a3f4864dbaff7b424b523e3c7e9f4dbf8
tbottini <thomasbottini@protonmail.com>
2022-09-18 18:48:58 +0200

    add script for installing postgres container

---
+++ infra/docker/create-postgres-container.bash
@@ -0,0 +1,7 @@
+docker run \
+    --name postgresql \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=psql 
+    -p 5432:5432 \
+    -v $POSTGRESQL_LOCAL_PATH:/var/lib/postgresql/data \
+    -d postgres
