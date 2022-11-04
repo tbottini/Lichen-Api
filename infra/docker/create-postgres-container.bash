95eb7c11de01d853fa7fb2ca02f23418be773e2d
tbottini <thomasbottini@protonmail.com>
2022-11-04 13:14:51 +0100

    add greenlock for ssl

--- infra/docker/create-postgres-container.bash
+++ infra/docker/create-postgres-container.bash
@@ -1,9 +1,8 @@
-docker build . --tag lichen_api
+# docker build . --tag lichen_api
 
-docker run \
-    --name postgresql \
+docker run --name postgresql \
     -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=psql 
+    -e POSTGRES_PASSWORD=psql \
     -p 5432:5432 \
     -v $POSTGRESQL_LOCAL_PATH:/var/lib/postgresql/data \
     -d postgres
