014de4257e9569823bf23104c2b4e940150a4e70
tbottini <thomasbottini@protonmail.com>
2022-11-06 00:45:15 +0100

    add ci environement for tests

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,12 +1,14 @@
 const execShPromise = require('exec-sh').promise
 const config = require('config')
 
+console.log(process.env.NODE_ENV, config)
+
 const CMD_RUN = 'npm run'
 const CMD_LAUNCH = 'npm'
 const CMD_EXEC = 'npx'
 
 function npx(command) {
-  return CMD_EXEC + ' ' + command;
+  return CMD_EXEC + ' ' + command
 }
 
 function installDevDep(dep) {
@@ -14,12 +16,12 @@ function installDevDep(dep) {
 }
 
 function sudo(cmd) {
-  return 'sudo ' + cmd;
+  return 'sudo ' + cmd
 }
 
 function sh(cmd) {
-  console.log(cmd);
-  return execShPromise(cmd);
+  console.log(cmd)
+  return execShPromise(cmd)
 }
 
 async function prismaMigrate(databaseUrl) {
@@ -36,19 +38,18 @@ export function applyMigration() {
 
 export function generatePrismaClient() {
   // sudo(installDevDep('prisma@latest'))
-  return [npx('prisma generate'), sudo(installDevDep('@prisma/client@latest'))].join(' && ')
+  return [
+    npx('prisma generate'),
+    sudo(installDevDep('@prisma/client@latest')),
+  ].join(' && ')
 }
 
-export function getDatabaseEnv(databaseUrl: string)  {
+export function getDatabaseEnv(databaseUrl: string) {
   return 'DATABASE_URL="' + databaseUrl + '"'
 }
 
 const commands = [applyMigration()]
 
-
-
 console.log(config.database.url)
 
-  sh(
-    sudo(getDatabaseEnv(config.database.url) + ' ' + commands.join( ' && ')))
-
+sh(sudo(getDatabaseEnv(config.database.url) + ' ' + commands.join(' && ')))
