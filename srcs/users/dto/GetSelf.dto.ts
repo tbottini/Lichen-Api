70969a947392b77e05d1056f0195903463f74934
tbottini <thomasbottini@protonmail.com>
2022-12-11 01:06:08 +0100

    test that get self return the user position

--- srcs/users/dto/GetSelf.dto.ts
+++ srcs/users/dto/GetSelf.dto.ts
@@ -2,6 +2,6 @@ import { Position } from '../../interfaces/Position.type'
 import { IncludesUsers, UserBase } from '../repositories/Users.scope'
 
 export type GetSelfDto = UserBase & {
-  defaultPosition: Position | null
+  position: Position | null
   email: string
 } & IncludesUsers
