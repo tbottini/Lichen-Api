d02f2ef4a47f4966fb82062e047f989ec1a51aa6
tbottini <thomasbottini@protonmail.com>
2023-10-14 23:33:07 +0200

    remove comments

--- srcs/modules/auth.ts
+++ srcs/modules/auth.ts
@@ -34,26 +34,6 @@ export function isAuthorized(header: string, ownerId: number) {
   return false
 }
 
-// export function configureMiddleware(getOwnerId: (req) => number) {
-//   return (req, res, next) =>
-//     middlewareAuthorization(getOwnerId(req), req, res, next)
-// }
-
-// function middlewareAuthorization(ownerId: number, req: any, res, next) {
-//   const token = extractTokenFromHeader(req.headers.authorization)
-
-//   if (!isAuthorized(token, ownerId)) {
-//     return res.status(401).end('Unauthorized')
-//   }
-
-//   const user = verify(token)
-
-//   req.user = {
-//     id: user.id,
-//   }
-//   next()
-// }
-
 export function forbiden(res) {
   return res.status(401).end('Forbiden')
 }
