d90ed3febb2fb90631b0530fb4904b04fc178197
tbottini <thomasbottini@protonmail.com>
2023-11-05 20:46:12 +0100

    add test for template generation

--- srcs/modules/email/MjmlTemplateRepository.ts
+++ srcs/modules/email/MjmlTemplateRepository.ts
@@ -2,14 +2,16 @@ import fs from 'fs'
 import { logger } from '../logger'
 import { MjmlMailTemplate } from './MjmlMailTemplate'
 
+export function createProductionMjmlTemplateRepository(assetFolder: string) {
+  return new MjmlTemplateRepository(new MjmlTemplateFileReader(assetFolder))
+}
+
 export class MjmlTemplateRepository {
   private readonly defaultWrapper!: MjmlMailTemplate
-  private readonly assetFolder: string
 
   templates: Record<string, MjmlMailTemplate> = {}
 
-  constructor(assetFolder: string) {
-    this.assetFolder = assetFolder
+  constructor(private readonly fileReader: MjmlTemplateLoader) {
     this.defaultWrapper = this.generateDefaultWrapper({})
   }
 
@@ -20,7 +22,7 @@ export class MjmlTemplateRepository {
     linkSoundcloud,
     linkTwitter,
   }: WrapperOptions): MjmlMailTemplate {
-    logger.info('MjmlFile - Basic Template was generate')
+    // logger.info('MjmlFile - Basic Template was generate')
     return new MjmlMailTemplate(this.readMjml('basic'))
       .set('link-facebook', linkFacebook)
       .set('link-instagram', linkInstagram)
@@ -37,16 +39,14 @@ export class MjmlTemplateRepository {
   }
 
   getTemplate(templateName: string): MjmlMailTemplate {
+    if (!this.templates[templateName]) {
+      throw new Error(`Template with name : ${templateName} doesn't exists`)
+    }
     return this.templates[templateName].duplicate()
   }
 
-  private readMjml(templateFilename) {
-    if (this.assetFolder == null) {
-      throw new Error(`No folder was provide for mjml file ${templateFilename}`)
-    }
-    return fs
-      .readFileSync(this.assetFolder + templateFilename + '.mjml')
-      .toString()
+  private readMjml(templateFilename: string) {
+    return this.fileReader.readMjml(templateFilename)
   }
 }
 
@@ -57,3 +57,20 @@ type WrapperOptions = Partial<{
   linkSoundcloud: string
   linkTwitter: string
 }>
+
+export interface MjmlTemplateLoader {
+  readMjml(templateFilename: string): string
+}
+
+class MjmlTemplateFileReader implements MjmlTemplateLoader {
+  constructor(private readonly assetFolder: string) {}
+
+  readMjml(templateFilename: string) {
+    if (this.assetFolder == null) {
+      throw new Error(`No folder was provide for mjml file ${templateFilename}`)
+    }
+    return fs
+      .readFileSync(this.assetFolder + templateFilename + '.mjml')
+      .toString()
+  }
+}
